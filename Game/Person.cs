253e49a78c66ce61ada0b892203ba490f013a1ed
samfromdeath <samuel@intelogy.com.au>
2017-10-18 16:48:50 +1100

    Add project files.

---
+++ Game/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Person : Entity
+    {
+        public decimal Age;
+        public const decimal MinAge = 15;
+        public const decimal MaxAge = 80;
+
+        public Person()
+        {
+            Age = MinAge;
+        }
+    }
+}
